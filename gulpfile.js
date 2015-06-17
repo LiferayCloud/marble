3062bfdc101b8217209534d1ade2c038deb453fc
Zeno Rocha <hi@zenorocha.com>
2015-06-17 14:49:49 -0700

    Renames sass task

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 
-gulp.task('sass', function() {
+gulp.task('build', function() {
 	return gulp.src('src/*.scss')
 		.pipe(sass())
 		.pipe(gulp.dest('build'));

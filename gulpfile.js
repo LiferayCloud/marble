2ebd05cdbf8d9dd9482cc7495c2c817e57d8f128
Zeno Rocha <hi@zenorocha.com>
2016-09-19 11:12:17 -0700

    Removes concatenation and minify

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,22 @@
 var gulp = require('gulp');
-var cleanCSS = require('gulp-clean-css');
-var concat = require('gulp-concat');
 var iconfont = require('gulp-iconfont');
 var iconfontCss = require('gulp-iconfont-css');
 var sass = require('gulp-sass');
-var del = require('del');
-var runSequence = require('run-sequence');
 
 // All -------------------------------------------------------------------------
 
-gulp.task('default', function(callback) {
-  runSequence(['bootstrap', 'fonts'], 'optimize', callback);
-});
+gulp.task('default', ['bootstrap', 'fonts']);
 
 // Bootstrap -------------------------------------------------------------------
+
+gulp.task('watch', function() {
+	gulp.watch('src/*.scss', ['bootstrap']);
+});
 
 gulp.task('bootstrap', function() {
 	return gulp.src('src/*.scss')
 		.pipe(sass({includePaths: ['node_modules']}))
 		.pipe(gulp.dest('build'));
-});
-
-gulp.task('watch', function() {
-	gulp.watch('src/*.scss', ['bootstrap']);
 });
 
 // Fonts -----------------------------------------------------------------------
@@ -64,26 +58,4 @@ gulp.task('font-icon-16', function() {
 gulp.task('font-galano', function () {
 	return gulp.src(['src/fonts/galano/*'])
 		.pipe(gulp.dest('build/fonts/galano'));
-});
-
-// Optimization ----------------------------------------------------------------
-
-gulp.task('concat', function() {
-  return gulp.src('build/**/*.css')
-    .pipe(concat('westyle.css'))
-    .pipe(gulp.dest('build'));
-});
-
-gulp.task('minify', function() {
-  return gulp.src('build/westyle.css')
-    .pipe(cleanCSS())
-    .pipe(gulp.dest('build'));
-});
-
-gulp.task('clean', function () {
-  return del(['build/bootstrap.css']);
-});
-
-gulp.task('optimize', function(callback) {
-  runSequence('concat', 'minify', 'clean', callback);
 });

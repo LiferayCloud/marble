b7fca6b5a907af901995ed2fb6e3355d21328372
ygor.costa <ygor.costa@liferay.com>
2017-01-20 09:33:03 -0800

    Generate file structure using Electric

---
+++ packages/marblecss.com/gulpfile.js
@@ -0,0 +1,68 @@
+'use strict';
+
+const connect = require('gulp-connect');
+const electric = require('electric');
+const ghPages = require('gulp-gh-pages');
+const gulp = require('gulp');
+const runSequence = require('run-sequence');
+const sass = require('gulp-sass');
+
+electric.registerTasks({
+	gulp: gulp,
+	plugins: ['electric-components']
+});
+
+// CSS -------------------------------------------------------------------------
+
+gulp.task('css', () => {
+	return gulp.src('src/styles/**/*.scss')
+		.pipe(sass({includePaths: ['node_modules']}))
+		.pipe(gulp.dest('dist/styles'));
+});
+
+// Fonts -----------------------------------------------------------------------
+
+gulp.task('fonts', () => {
+	return gulp.src('node_modules/westyle/build/fonts/**')
+		.pipe(gulp.dest('dist/fonts'));
+});
+
+// Server ----------------------------------------------------------------------
+
+gulp.task('server', () => {
+	connect.server({
+		root: 'dist',
+		port: 8888
+	});
+});
+
+// Deploy ----------------------------------------------------------------------
+
+gulp.task('wedeploy', () => {
+	return gulp.src('src/container.json')
+		.pipe(gulp.dest('dist'));
+});
+
+gulp.task('deploy', ['default'], () => {
+	return gulp.src('dist/**/*')
+		.pipe(ghPages({
+			branch: 'wedeploy'
+		}));
+});
+
+// Watch -----------------------------------------------------------------------
+
+gulp.task('watch', () => {
+	gulp.watch('src/**/*.+(md|soy|js|fm)', ['generate']);
+	gulp.watch('src/styles/**/*.scss', ['css']);
+});
+
+// Build -----------------------------------------------------------------------
+
+gulp.task('build', (callback) => {
+	runSequence('generate', ['css', 'fonts', 'wedeploy'], callback);
+});
+
+gulp.task('default', (callback) => {
+	runSequence('build', 'server', 'watch', callback);
+});

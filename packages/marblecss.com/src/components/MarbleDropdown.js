121db7cbd635054b4ab12d254369a55f6423ff2c
ygor.costa <ygor.costa@liferay.com>
2017-01-26 11:50:12 -0800

    Adds metal-components/dropdown

---
+++ packages/marblecss.com/src/components/MarbleDropdown.js
@@ -0,0 +1,15 @@
+'use strict';
+
+import Component from 'metal-component';
+import Soy from 'metal-soy';
+import 'metal-dropdown';
+
+import templates from './MarbleDropdown.soy';
+
+class MarbleDropdown extends Component {
+	
+};
+
+Soy.register(MarbleDropdown, templates);
+
+export default MarbleDropdown;

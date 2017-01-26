7196a2134da9522a13a418fd1ffefe817272b13d
ygor.costa <ygor.costa@liferay.com>
2017-01-26 14:30:01 -0800

    Adds metal-components/tooltip

---
+++ packages/marblecss.com/src/pages/docs/metal-components/tooltip.js
@@ -0,0 +1,22 @@
+'use strict';
+
+import { core, string } from 'metal';
+import Component from 'metal-component';
+import Soy from 'metal-soy';
+import 'metal-tooltip';
+
+import templates from './tooltip.soy';
+
+class tooltip extends Component {
+
+	tooltipAlignElementChanged(event) {
+		const value = event.newVal.getAttribute('alt');
+		if (value) {
+			this.components.tooltip.title = unescape(value);
+		}
+	}
+};
+
+Soy.register(tooltip, templates);
+
+export default tooltip;

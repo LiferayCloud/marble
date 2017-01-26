31f68f93c8d512534ebf22c9d010253462e44524
ygor.costa <ygor.costa@liferay.com>
2017-01-26 13:27:05 -0800

    Adds metal-component/select

---
+++ packages/marblecss.com/src/components/MarbleSelect.js
@@ -0,0 +1,15 @@
+'use strict';
+
+import Component from 'metal-component';
+import Soy from 'metal-soy';
+import 'metal-select';
+
+import templates from './MarbleSelect.soy';
+
+class MarbleSelect extends Component {
+	
+};
+
+Soy.register(MarbleSelect, templates);
+
+export default MarbleSelect;

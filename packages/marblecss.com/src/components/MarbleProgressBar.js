4a3624c522a289855f2b7b487f4c37a1704d7ba4
ygor.costa <ygor.costa@liferay.com>
2017-01-26 11:50:48 -0800

    Adds metal-component/progress-bar

---
+++ packages/marblecss.com/src/components/MarbleProgressBar.js
@@ -0,0 +1,15 @@
+'use strict';
+
+import Component from 'metal-component';
+import Soy from 'metal-soy';
+import 'metal-progressbar';
+
+import templates from './MarbleProgressBar.soy';
+
+class MarbleProgressBar extends Component {
+	
+};
+
+Soy.register(MarbleProgressBar, templates);
+
+export default MarbleProgressBar;

1a23c99391c4d7bbeada539c137206330ba422b5
ygor.costa <ygor.costa@liferay.com>
2017-01-26 18:28:07 -0800

    Adds metal-component/toast

---
+++ packages/marblecss.com/src/components/MarbleToast.js
@@ -0,0 +1,15 @@
+'use strict';
+
+import Component from 'metal-component';
+import Soy from 'metal-soy';
+import 'metal-toast';
+
+import templates from './MarbleToast.soy';
+
+class MarbleToast extends Component {
+	
+};
+
+Soy.register(MarbleToast, templates);
+
+export default MarbleToast;

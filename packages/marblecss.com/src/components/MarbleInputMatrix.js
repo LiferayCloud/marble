f8d22a7194f6fe44fdaa3235769f9a0c854abcd2
ygor.costa <ygor.costa@liferay.com>
2017-01-26 18:27:40 -0800

    Adds metal-component/input-matrix

---
+++ packages/marblecss.com/src/components/MarbleInputMatrix.js
@@ -0,0 +1,15 @@
+'use strict';
+
+import Component from 'metal-component';
+import Soy from 'metal-soy';
+import 'metal-input-matrix';
+
+import templates from './MarbleInputMatrix.soy';
+
+class MarbleInputMatrix extends Component {
+	
+};
+
+Soy.register(MarbleInputMatrix, templates);
+
+export default MarbleInputMatrix;

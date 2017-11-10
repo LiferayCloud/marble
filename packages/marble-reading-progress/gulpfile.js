1b5b13a92598e95a3e79ef05586e6014e084a25d
Zeno Rocha <zno.rocha@gmail.com>
2017-11-10 11:37:10 -0800

    Migrates metal-reading-progress to marble

---
+++ packages/marble-reading-progress/gulpfile.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var metal = require('gulp-metal');
+
+metal.registerTasks({
+	bundleCssFileName: 'readingProgress.css',
+	bundleFileName: 'readingProgress.js',
+	moduleName: 'metal-reading-progress'
+});

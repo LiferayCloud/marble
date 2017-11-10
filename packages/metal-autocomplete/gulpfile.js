93ba276b596d0e96d570ca758c9a2aad750071e6
Zeno Rocha <zno.rocha@gmail.com>
2017-11-09 17:29:57 -0800

    Migrates metal-autocomplete to marble

---
+++ packages/metal-autocomplete/gulpfile.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var metal = require('gulp-metal');
+
+metal.registerTasks({
+	bundleCssFileName: 'autocomplete.css',
+	bundleFileName: 'autocomplete.js',
+	moduleName: 'metal-autocomplete'
+});

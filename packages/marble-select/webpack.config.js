d339aa589ed3aab56deaf110b71b8f680610c23e
Zeno Rocha <zno.rocha@gmail.com>
2017-11-13 15:29:05 -0800

    Updates select component demonstration

--- packages/marble-select/webpack.config.js
+++ packages/marble-select/webpack.config.js
@@ -1,27 +1,27 @@
 const webpack = require('webpack');
 
 module.exports = {
-	entry: './src/Select.js',
-	module: {
-		rules: [{
-			test: /\.js$/,
-			exclude: /(node_modules)/,
-			use: {
-				loader: 'babel-loader',
-				options: {
-					compact: false,
-					presets: ['babel-preset-es2015'],
-					plugins: ['babel-plugin-transform-node-env-inline']
-				}
-			}
-		}]
-	},
-	output: {
-		library: 'metal',
-		libraryTarget: 'this',
-		filename: './build/globals/select.js'
-	},
-	plugins: [
-		new webpack.optimize.ModuleConcatenationPlugin()
-	]
+  entry: './src/Select.js',
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: /(node_modules)/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          compact: false,
+          presets: ['babel-preset-es2015'],
+          plugins: ['babel-plugin-transform-node-env-inline']
+        }
+      }
+    }]
+  },
+  output: {
+    library: 'metal',
+    libraryTarget: 'this',
+    filename: './build/globals/marble-select.js'
+  },
+  plugins: [
+    new webpack.optimize.ModuleConcatenationPlugin()
+  ]
 };

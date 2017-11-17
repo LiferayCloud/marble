c64663208bfdda886bb5f74bcf43e3a0fad25b7e
Zeno Rocha <zno.rocha@gmail.com>
2017-11-17 15:56:56 -0800

    Adds tests and generates snapshots

--- packages/marble-topbar/test/Topbar.js
+++ packages/marble-topbar/test/Topbar.js
@@ -16,4 +16,64 @@ describe('Topbar', function() {
 
     expect(topbar).toMatchSnapshot();
   });
+
+  it('should render a topbar light', () => {
+    topbar = new Topbar({
+      theme: 'topbar topbar-light'
+    });
+
+    expect(topbar).toMatchSnapshot();
+  });
+
+  it('should render a logo with text', () => {
+    topbar = new Topbar({
+      logo: {
+        text: 'Marble'
+      }
+    });
+
+    expect(topbar).toMatchSnapshot();
+  });
+
+  it('should render a logo with icon', () => {
+    topbar = new Topbar({
+      logo: {
+        icon: 'icon-12-github'
+      }
+    });
+
+    expect(topbar).toMatchSnapshot();
+  });
+
+  it('should render a logo with icon and text', () => {
+    topbar = new Topbar({
+      logo: {
+        text: 'Marble',
+        icon: 'icon-12-github'
+      }
+    });
+
+    expect(topbar).toMatchSnapshot();
+  });
+
+  it('should render a logo with image', () => {
+    topbar = new Topbar({
+      logo: {
+        image: 'logo.svg'
+      }
+    });
+
+    expect(topbar).toMatchSnapshot();
+  });
+
+  it('should render a logo with image and text', () => {
+    topbar = new Topbar({
+      logo: {
+        text: 'Marble',
+        image: 'logo.svg'
+      }
+    });
+
+    expect(topbar).toMatchSnapshot();
+  });
 });

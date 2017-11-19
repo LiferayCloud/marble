abdf17ab966bca96ee7c649ab0ed24988b496cf7
Zeno Rocha <zno.rocha@gmail.com>
2017-11-18 20:27:17 -0800

    Publish marble-spinner v1.0.0

--- packages/marble-spinner/src/Spinner.soy.js
+++ packages/marble-spinner/src/Spinner.soy.js
@@ -24,7 +24,9 @@ var soyIdom = goog.require('soy.idom');
 
 /**
  * @param {{
- *  id: (!goog.soy.data.SanitizedContent|null|string|undefined)
+ *  isDone: (boolean|null|undefined),
+ *  size: (!goog.soy.data.SanitizedContent|null|string|undefined),
+ *  style: (!goog.soy.data.SanitizedContent|null|string|undefined)
  * }} opt_data
  * @param {Object<string, *>=} opt_ijData
  * @param {Object<string, *>=} opt_ijData_deprecated
@@ -34,18 +36,29 @@ var soyIdom = goog.require('soy.idom');
 function $render(opt_data, opt_ijData, opt_ijData_deprecated) {
   opt_ijData = opt_ijData_deprecated || opt_ijData;
   opt_data = opt_data || {};
+  /** @type {boolean|null|undefined} */
+  var isDone = soy.asserts.assertType(opt_data.isDone == null || (goog.isBoolean(opt_data.isDone) || opt_data.isDone === 1 || opt_data.isDone === 0), 'isDone', opt_data.isDone, 'boolean|null|undefined');
   /** @type {!goog.soy.data.SanitizedContent|null|string|undefined} */
-  var id = soy.asserts.assertType(opt_data.id == null || (goog.isString(opt_data.id) || opt_data.id instanceof goog.soy.data.SanitizedContent), 'id', opt_data.id, '!goog.soy.data.SanitizedContent|null|string|undefined');
-  incrementalDom.elementOpenStart('div');
-      incrementalDom.attr('id', id);
+  var size = soy.asserts.assertType(opt_data.size == null || (goog.isString(opt_data.size) || opt_data.size instanceof goog.soy.data.SanitizedContent), 'size', opt_data.size, '!goog.soy.data.SanitizedContent|null|string|undefined');
+  /** @type {!goog.soy.data.SanitizedContent|null|string|undefined} */
+  var style = soy.asserts.assertType(opt_data.style == null || (goog.isString(opt_data.style) || opt_data.style instanceof goog.soy.data.SanitizedContent), 'style', opt_data.style, '!goog.soy.data.SanitizedContent|null|string|undefined');
+  var doneClass__soy7 = '';
+  doneClass__soy7 += isDone ? 'spinner-done' : '';
+  var sizeClass__soy14 = '';
+  sizeClass__soy14 += size ? 'spinner-' + size : '';
+  var styleClass__soy22 = '';
+  styleClass__soy22 += style ? 'spinner-' + style : '';
+  incrementalDom.elementOpenStart('span');
+      incrementalDom.attr('class', 'spinner ' + doneClass__soy7 + ' ' + sizeClass__soy14 + ' ' + styleClass__soy22);
   incrementalDom.elementOpenEnd();
-    incrementalDom.text('Hello World');
-  incrementalDom.elementClose('div');
+  incrementalDom.elementClose('span');
 }
 exports.render = $render;
 /**
  * @typedef {{
- *  id: (!goog.soy.data.SanitizedContent|null|string|undefined)
+ *  isDone: (boolean|null|undefined),
+ *  size: (!goog.soy.data.SanitizedContent|null|string|undefined),
+ *  style: (!goog.soy.data.SanitizedContent|null|string|undefined)
  * }}
  */
 $render.Params;
@@ -53,8 +66,8 @@ if (goog.DEBUG) {
   $render.soyTemplateName = 'Spinner.render';
 }
 
-exports.render.params = ["id"];
-exports.render.types = {"id":"string"};
+exports.render.params = ["isDone","size","style"];
+exports.render.types = {"isDone":"bool","size":"string","style":"string"};
 templates = exports;
 return exports;
 

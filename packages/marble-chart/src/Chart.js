4c45a9334778a8ed76758fe52dd36ef8faa40d3f
Zeno Rocha <zno.rocha@gmail.com>
2018-03-22 17:09:00 -0700

    Creates base chart component using Chart.js #91

--- packages/marble-chart/src/Chart.js
+++ packages/marble-chart/src/Chart.js
@@ -1,13 +1,38 @@
+import {isServerSide} from 'metal';
 import Component from 'metal-component';
 import Soy from 'metal-soy';
 import {Config} from 'metal-state';
+import ChartJS from 'chart.js';
 
 import templates from './Chart.soy.js';
 
 /**
  * Chart component.
  */
-class Chart extends Component {}
+class Chart extends Component {
+  attached() {
+    if (isServerSide()) {
+      return;
+    }
+
+    this.chart = new ChartJS(this.refs.canvas, {
+      type: this.type,
+      data: this.data,
+      options: this.options,
+    });
+  }
+
+  willReceiveState(changes) {
+    if (changes.data) {
+      this.chart.data = changes.data.newVal;
+      this.chart.update();
+    }
+  }
+
+  disposed() {
+    this.chart.destroy();
+  }
+}
 
 /**
  * State definition.
@@ -16,11 +41,28 @@ class Chart extends Component {}
  */
 Chart.STATE = {
   /**
-   * ID to be applied to the element.
+   * Built-in types for Chart.js
    * @type {!String}
-   * @default example
    */
-  id: Config.string().value('example'),
+  type: {
+    value: Config.oneOf(['bar', 'bubble', 'line', 'pie', 'polarArea', 'radar']),
+  },
+
+  /**
+   * Data used to render Chart.js
+   * @type {!Object}
+   */
+  data: {
+    value: {}
+  },
+
+  /**
+   * Styling options for Chart.js
+   * @type {!Object}
+   */
+  options: {
+    value: {}
+  }
 };
 
 Soy.register(Chart, templates);

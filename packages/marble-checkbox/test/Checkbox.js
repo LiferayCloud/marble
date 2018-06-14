74d67ee9631b01b9620c9acc24eeb7488aff05c2
Zeno Rocha <zno.rocha@gmail.com>
2018-06-13 19:52:49 -0700

    Adds tests for default, checked, and unchecked

--- packages/marble-checkbox/test/Checkbox.js
+++ packages/marble-checkbox/test/Checkbox.js
@@ -14,4 +14,25 @@ describe('Checkbox', () => {
 
     expect(checkbox).toMatchSnapshot();
   });
+
+  it('should be unchecked', () => {
+    checkbox = new Checkbox({
+      id: 'checkbox-1',
+      label: 'Option 1',
+      value: 'Checkbox 1',
+    });
+
+    expect(checkbox).toMatchSnapshot();
+  });
+
+  it('should be checked', () => {
+    checkbox = new Checkbox({
+      checked: true,
+      id: 'checkbox-2',
+      label: 'Option 2',
+      value: 'Checkbox 2',
+    });
+
+    expect(checkbox).toMatchSnapshot();
+  });
 });
